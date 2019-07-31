2ace16a18d8e5a8d07e35e3e530e9ed998e8ff8a
R0zetka <denis.kr2001@gmail.com>
2019-07-31 21:39:03 +0300

    0

---
+++ New Unity Project (1)/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float speedJump;
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        
+
+
+    }
+}                                                       
+
