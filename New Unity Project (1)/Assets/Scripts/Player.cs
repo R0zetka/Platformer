dd73399e563f8e39a3e22d241dc9a55f0ea8615b
R0zetka <denis.kr2001@gmail.com>
2019-08-01 15:52:37 +0300

    op

--- New Unity Project (1)/Assets/Scripts/Player.cs
+++ New Unity Project (1)/Assets/Scripts/Player.cs
@@ -1,36 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using CnControls;
+
 
 public class Player : MonoBehaviour
 {
-    public float speed;
-    public float speedJump;
-    public GameObject player;
+    public float speed;//скорость персонажа
+    public float speedJump;//сила прыжка
+    public float gravityForce; // гравитация персонажа
+    public Vector3 moveVector; // направление движения персонажа
+
+    // ссылка на компоненты
+    private CharacterController ch_controller;
+    private Animator ch_animator;
+
+
+
+
+    public GameObject player;//персонаж
 
     // Use this for initialization
     void Start()
     {
-
+        ch_controller = GetComponent<CharacterController>();
+        ch_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-        {
+        CharacterMove();
+        GamingGravity();
+    }
+    //метод перемещения
+    private void CharacterMove()
+    {
+        //перемещение
+        moveVector = Vector3.zero;
+        moveVector.x = Input.GetAxis("Horizontal") * speed;
+        moveVector.y = gravityForce;
 
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        }
-
-        
+        ch_controller.Move(moveVector * Time.deltaTime);// метод движения персонажа по  направлению
 
 
     }
+
+    //метод гравитации
+    private void GamingGravity()
+    {
+        if (!ch_controller.isGrounded)
+        {
+            gravityForce -= 20f * Time.deltaTime;
+        }
+        else
+        {
+            gravityForce = -1f;
+        }
+    }
+
+
+
 }                                                       
 
