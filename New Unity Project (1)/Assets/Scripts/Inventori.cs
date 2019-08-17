4b87d86e083a62275eeecac2944cfe8e770722c0
R0zetka <denis.kr2001@gmail.com>
2019-08-17 12:51:03 +0300

    кнопка инвенторя

---
+++ New Unity Project (1)/Assets/Scripts/Inventori.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventori : MonoBehaviour
+{
+    public GameObject Button;
+    float Button1;
+    public Camera Camera1;
+    public Camera Camera2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button1 = Button.transform.position.y;
+        Camera1.enabled = true;
+        Camera2.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Button1 != Button.transform.position.y)
+        {
+            Camera1.enabled = !Camera1.enabled;
+            Camera2.enabled = !Camera2.enabled;
+        }
+    }
+    
+}
